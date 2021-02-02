41e1b9b8cc31d97e8fe4a65f869e07a25ae7691c
Nazar Pidhirnyi <nazar.pidhirnyi@gmail.com>
2021-02-02 12:27:42 +0200

    feat: add reverse int function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,9 @@
-module.exports = function reverse (n) {
-  
+module.exports = function reverse(n) {
+  let reversedInt
+  if (n < 0) {
+    n = n * (-1)
+    reversedInt = n.toString().split('').reverse().join('')
+  }
+  reversedInt = n.toString().split('').reverse().join('')
+  return reversedInt
 }
